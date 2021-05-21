4aca37cef059ec3884360e1e160d2fce5d4df0f6
Lialix <81411512+Lialix@users.noreply.github.com>
2021-05-21 10:35:27 +0200

    Create cavalier.java

---
+++ cavalier.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+
+public class cavalier extends piece{
+
+
+  public cavalier(boolean c) {
+    super(c);
+  }
+
+
+
+
+
+  public cavalier(cavalier c){
+    super(c);
+  }
+
+
+
+
+
+  public String toString(){
+    return "3";
+  }
+
+  public int typeMouvement(int xPiece, int yPiece, int xDestination, int yDestination){
+    if ((xDestination == xPiece-1 && (yDestination == yPiece-2 || yDestination == yPiece+2))
+     || (xDestination == xPiece+1 && (yDestination == yPiece-2 || yDestination == yPiece+2))
+     || (xDestination == xPiece-2 && (yDestination == yPiece-1 || yDestination == yPiece+1))
+     || (xDestination == xPiece+2 && (yDestination == yPiece-1 || yDestination == yPiece+1)))
+      // mouvement qui ne requiert pas de check les pieces sur le chemin
+      return 1;
+    return 0;
+  }
+
+
+
+
+
+
+  public Image getImage(){
+    if (this.couleur)
+      return Toolkit.getDefaultToolkit().getImage("img/WhiteKnight.png");
+    else
+      return Toolkit.getDefaultToolkit().getImage("img/BlackKnight.png");
+  }
+
+
+
+}
