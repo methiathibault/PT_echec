73805e1b47b18066db098ae8f5aa8f3fa6204e10
methiathibault <78618802+methiathibault@users.noreply.github.com>
2021-05-21 10:56:15 +0200

    Update pion.java

--- pion.java
+++ pion.java
@@ -26,7 +26,7 @@ public class pion extends piece{
     if (xDestination == xPiece && yDestination - yPiece == avant)
       //mouvement en avant qui requiert qu'il n'y ai pas d'ennemi sur la case de destination
       return 2;
-    else if ((xDestination == xPiece && yDestination - yPiece == 2*avant) && !this.aEffectueUnMouvement)
+    else if ((xDestination == xPiece && yDestination - yPiece == 2*avant) && !this.aFaitDeplacement)
       //mouvement qui requiert que le pion n'ai pas deja effectué un mouvement et qu'il n'y ai pas d'enemi sur le chemin
       return 3;
     else if ((xDestination - xPiece == -1 && yDestination - yPiece == avant) || (xDestination - xPiece == 1 && yDestination - yPiece == avant))
@@ -43,9 +43,9 @@ public class pion extends piece{
 
   public Image getImage(){
     if (this.couleur)
-      return Toolkit.getDefaultToolkit().getImage("img/WhitePawn.png");
+      return Toolkit.getDefaultToolkit().getImage("img/PionBlanc.png");
     else
-      return Toolkit.getDefaultToolkit().getImage("img/BlackPawn.png");
+      return Toolkit.getDefaultToolkit().getImage("img/PionNoir.png");
   }
 
 
